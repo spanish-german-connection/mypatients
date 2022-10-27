9bf536c650089cd3c06f33ac0a138cf11da09217
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-27 11:00:39 +0200

    update user model

--- models/User.model.js
+++ models/User.model.js
@@ -3,6 +3,10 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+    },
     email: {
       type: String,
       required: [true, "Email is required."],
@@ -14,10 +18,33 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required."],
     },
-    name: {
+    role: {
       type: String,
-      required: [true, "Name is required."],
+      enum: ["therapist", "secretary", "admin"],
+      lowercase: true,
+      default: "therapist",
     },
+    patients: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Patient",
+        default: "No patients yet",
+      },
+    ],
+    appointments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Appointment",
+        default: "No appointments yet",
+      },
+    ],
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+        default: "No posts yet",
+      },
+    ],
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
